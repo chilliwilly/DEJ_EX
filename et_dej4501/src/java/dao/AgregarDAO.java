48323557bc07127e3cc0325490a8f272a8351a0e
Williams <w.contreras.f@gmail.com>
2014-12-11 01:28:56 -0300

    Metodos DAO Nuevos
    se agregan metodo terminado de retorno de los ID Reg de los
    participantes y se agrega metodo sin terminar del agregar paticipante

--- et_dej4501/src/java/dao/AgregarDAO.java
+++ et_dej4501/src/java/dao/AgregarDAO.java
@@ -15,23 +15,51 @@ public class AgregarDAO {
     
     public ArrayList<Raza> listaRaza()
     {
-        ArrayList<Raza> listado = new ArrayList<Raza>();
+        ArrayList<Raza> listado = new ArrayList<>();
         
         String sql = "SELECT * FROM RAZA ORDER BY ID_RAZA ASC;";
         try (PreparedStatement stmt = cnx.prepareStatement(sql)){
-            ResultSet rs = stmt.executeQuery(sql);
-            
-            while(rs.next())
+            try(ResultSet rs = stmt.executeQuery())
             {
-                int num = rs.getInt("ID_RAZA");
-                String nom = rs.getString("NOMBRE_RAZA");                
-                
-                Raza raza = new Raza(num, nom);
-                listado.add(raza);
+                while(rs.next())
+                {
+                    int num = rs.getInt("ID_RAZA");
+                    String nom = rs.getString("NOMBRE_RAZA");                
+
+                    Raza raza = new Raza(num, nom);
+                    listado.add(raza);
+                }
             }
         } catch (Exception ex) {
             throw new RuntimeException("Error al Buscar Raza", ex);
         }
         return listado;
+    }
+    
+    public void agregarParicipante(Participante p)
+    {
+    
+    }
+    
+    public ArrayList<Integer> listaRegParticipante()
+    {
+        ArrayList<Integer> lsIdReg = new ArrayList<>();
+        
+        String sql = "SELECT ID_PARTICIPANTE FROM PARTICIPANTE;";
+        try (PreparedStatement stmt = cnx.prepareStatement(sql)){
+            try(ResultSet rs = stmt.executeQuery())
+            {
+                while(rs.next())
+                {
+                    int num = rs.getInt("ID_PARTICIPANTE");
+
+                    lsIdReg.add(num);
+                }
+            }
+        } catch (Exception ex) {
+            throw new RuntimeException("Error al Buscar Reg Participante", ex);
+        }
+        
+        return lsIdReg;
     }
 }
